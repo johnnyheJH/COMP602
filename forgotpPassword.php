f22a4ef84df3b3376f973e0fc6f14be64015fd3c
Fahad Alajmi <alajmi771@gmail.com>
2018-09-24 16:38:06 +1200

    Add files via upload

---
+++ forgotpPassword.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$sessData = !empty($_SESSION['sessData'])?$_SESSION['sessData']:'';
+if(!empty($sessData['status']['msg'])){
+    $statusMsg = $sessData['status']['msg'];
+    $statusMsgType = $sessData['status']['type'];
+    unset($_SESSION['sessData']['status']);
+}
+?>
